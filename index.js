7a32a90e1ef6bfc46ef75cecb2e0b75b1b6cd810
Haoran Zhang <31876749+Zhang-Haoran@users.noreply.github.com>
2021-05-26 13:50:17 +1000

    set up to write react Component

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import App from "./App";
 
 ReactDom.render(
     (
-        // React.createElement('div',{},'hello world') //compile without babel.
-        <div>Hello world2</div> //need to compile with babel
+        //compile without babel.
+        // React.createElement('div',{},'hello world')
+
+        //need to compile with babel
+        // <div>Hello world2</div>
+        <App/>
     )
     ,document.querySelector('#app'))
