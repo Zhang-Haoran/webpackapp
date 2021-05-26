93a9c8707979099303319fa00ebab0673b71bf53
Haoran Zhang <31876749+Zhang-Haoran@users.noreply.github.com>
2021-05-26 13:25:35 +1000

    set up babel to use JSX

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-ReactDom.render((React.createElement('div',{},'hello world')),document.querySelector('#app'))
+ReactDom.render(
+    (
+        // React.createElement('div',{},'hello world') //compile without babel.
+        <div>Hello world2</div> //need to compile with babel
+    )
+    ,document.querySelector('#app'))
